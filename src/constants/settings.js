a21522946d9f97472dfc25cc9f2dca3dfb356705
Steve Good <sgood@lanctr.com>
2020-07-21 14:26:08 -0700

    Removed old components, updated settings.

--- src/constants/settings.js
+++ src/constants/settings.js
@@ -1,10 +1,7 @@
 
 const settings = {
   default: {
-    themeColor: 'dark',
-    cardStyle: 'images',
-    chipSize: 'medium',
-    builderOrientation: 'right'
+    themeColor: 'dark'
   },
   list: [
     {
@@ -14,22 +11,6 @@ const settings = {
         { key: 'dark', name: 'Dark Side' },
         { key: 'light', name: 'Light Side'},
         { key: 'blue', name: 'Fifth Trooper' }
-      ]
-    },
-    {
-      key: 'cardStyle',
-      name: 'Card Style',
-      values: [
-        { key: 'images', name: 'Images' },
-        { key: 'text', name: 'Text' }
-      ]
-    },
-    {
-      key: 'chipSize',
-      name: 'Chip Size',
-      values: [
-        { key: 'small', name: 'Small' },
-        { key: 'medium', name: 'Large' }
       ]
     }
   ]
