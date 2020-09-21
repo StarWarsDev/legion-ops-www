0b5787edc9b9c2dd85a0b622d7435d74b84b713f
Steve Good <sgood@lanctr.com>
2020-09-21 15:22:24 -0700

    Removed unused import

--- src/pages/Event/PublishButton.js
+++ src/pages/Event/PublishButton.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react"
+import React from "react"
 import { IconButton, Tooltip } from "@material-ui/core"
 import VisibilityIcon from "@material-ui/icons/Visibility"
 import VisibilityOffIcon from "@material-ui/icons/VisibilityOff"
